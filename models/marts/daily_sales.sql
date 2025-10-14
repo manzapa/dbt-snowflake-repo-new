4c2b158a3ab4c4f72a72e6b15999ff697f8e7e8b
Paul Marthy Manzano <paulmarthy@gmail.com>
2025-10-14 03:18:06 +0000

    1

--- models/marts/daily_sales.sql
+++ models/marts/daily_sales.sql
@@ -1,8 +1,8 @@
 
 select
     purchased_date,
-    sum(quantity) as no_items_sold,         -- total items sold per day
-    sum(quantity * price) as total_sales   -- total revenue per day
+    sum(quantity) as no_items_sold,         -- total items sold /day
+    sum(quantity * price) as total_sales   -- total rev /day
 from {{ ref('stg_orderitems') }}
 group by purchased_date
 order by purchased_date desc
